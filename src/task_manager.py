b0cee24f685774e59cf578f23f1280803a6037ce
GutoReis <guto.reisdelima@gmail.com>
2025-06-30 19:58:52 -0300

    feat(task_mngr): Add class to manage tasks.

---
+++ src/task_manager.py
@@ -0,0 +1,28 @@
+
+class TaskManager():
+    """
+    Manage all tasks and tasks operations for the Gantt View.
+    """
+
+    def __init__(self):
+        self.tasks_list = list()
+
+    def load_tasks(self, json_data: dict):
+        """Pass a JSON data to load the tasks.
+
+        Args:
+            json_data (dict): Task data for the Gantt.
+        """
+        pass
+
+    def new_task(self):
+        pass
+
+    def update_task(self):
+        pass
+
+    def delete_task(self):
+        pass
+
+    def locate_task_by_id(self):
+        pass
