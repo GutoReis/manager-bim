15ae8353a4a6b7271eeb06d8ec384af2b55d8443
Guto Reis <guto.reisdelima@gmail.com>
2026-03-18 22:58:38 -0300

    feat: Change icon path definition.
    As the ICON PATH was hard coded, the Freecad couldn't locate it, so it's necessary to change it to a 'dynamic' type, so freecad can properly locate it.

--- InitGui.py
+++ InitGui.py
@@ -1,31 +1,27 @@
-#import os
+from pathlib import Path
 
 import FreeCAD as App
 import FreeCADGui
-#from FreeCADGui import Workbench
+
+# from FreeCADGui import Workbench
+
 
 class ManagerBIMWorkbench(FreeCADGui.Workbench):
     """
     Defines the FreeCAD Workbench structure.
     """
-    
-    #ICON_PATH = os.path.join(
-    #    os.path.dirname(__file__),
-    #    "src",
-    #    "assets",
-    #    "logo_ManagerBim.svg"
-    #)
 
-    ICON_PATH = "/home/gutoreis/projects/manager-bim/src/assets/logo_ManagerBIM.svg"
-    Icon = ICON_PATH
+    ICON_PATH = Path(App.getResourceDir()) / "Mod" / "manager-bim" / "src" / "assets" / "logo_ManagerBIM.svg"
+    Icon = str(ICON_PATH)
     MenuText = "ManagerBIM Workbench"
     ToolTip = "Bancada dedicada para BIM 4D"
-    
+
     def Initialize(self):
         """
         This function is executed when FreeCAD starts.
         """
-        import src.workbench_commander 
+        import src.workbench_commander
+
         self.appendMenu("ManagerBIM", ["ManagerBimStart"])
 
     def Activated(self):
@@ -42,5 +38,6 @@ class ManagerBIMWorkbench(FreeCADGui.Workbench):
 
     def GetClassName(self):
         return "ManagerBIMWorkbench"
+
 
 FreeCADGui.addWorkbench(ManagerBIMWorkbench())
